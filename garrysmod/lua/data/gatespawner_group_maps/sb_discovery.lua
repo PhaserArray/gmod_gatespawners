52d3e6c44f624a12bb832dc575a1110c9390eaba
PhaserArray <me@phaserarray.me>
2023-02-07 23:35:21 +0200

    add sb_discovery gatespawner

---
+++ garrysmod/lua/data/gatespawner_group_maps/sb_discovery.lua
@@ -0,0 +1,169 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=stargate_sg1
+position=2704.511963 -5747.189453 -8210.570313
+angles=-0.044 179.962 -0.025
+address=SPAWNG
+group=M@
+name=Spawn
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=-6203.388184 8926.673828 -3490.680420
+angles=0.000 -26.085 0.000
+address=EXOTIC
+group=P@
+name=Purplar
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=11628.982422 408.549194 2389.605225
+angles=0.000 90.813 0.000
+address=WRECKD
+group=P@
+name=Culled World
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=1225.900024 10884.601563 -9132.000000
+angles=0.000 261.993 0.000
+address=CRYSTL
+group=P@
+name=Purplar's Moon
+private=true
+locale=false
+[stargate]
+classname=stargate_universe
+position=-2491.617676 -2089.416992 -2957.291992
+angles=0.000 -35.317 0.000
+address=SNOWVA
+group=U@!
+name=Snow Valley
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-6171.002441 -8612.163086 4555.750977
+angles=-1.835 -116.621 0.292
+address=DES3RT
+group=M@
+name=Desert World
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=6259.630371 5346.021973 6303.356445
+angles=-2.910 -19.900 -0.573
+address=DEATHW
+group=M@
+name=Doomed World
+private=false
+locale=false
+blocked=true
+[stargate]
+classname=stargate_atlantis
+position=5158.587402 -543.486572 3058.915771
+angles=0.691 67.113 -0.087
+address=S9ACEG
+group=P@
+name=Spacegate
+private=false
+locale=false
+__id=609
+[dhd]
+classname=dhd_concept
+position=2443.263916 -5754.694824 -8358.058594
+angles=0.000 -180.000 -0.000
+[dhd]
+classname=dhd_atlantis
+position=-5935.125977 8271.896484 -3535.649902
+angles=15.000 -73.023 -0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=12678.021484 502.612915 2262.003174
+angles=15.000 31.152 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-6302.112305 -8865.875000 4445.761230
+angles=-1.835 -116.621 0.292
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=6914.347656 5062.592773 6069.689941
+angles=-8.895 -22.186 0.458
+destroyed=true
+[dhd]
+classname=dhd_atlantis
+position=1077.475464 10781.635742 -9239.470703
+angles=15.000 -131.516 0.000
+destroyed=false
+[mobile_dhd]
+classname=mobile_dhd
+position=2059.601074 -5676.038574 -7980.952148
+angles=-0.000 -180.000 0.000
+model=models/props_lab/keypad.mdl
+[ring_panel]
+classname=ring_panel_ancient
+position=1716.316772 -7104.751465 -8312.869141
+angles=-24.229 90.000 -0.294
+[ring_base]
+classname=ring_base_ancient
+position=7158.498535 -12112.543945 -7120.011719
+angles=-0.000 28.801 0.000
+address=2
+[ring_base]
+classname=ring_base_ancient
+position=1717.366943 -6898.320313 -8347.621094
+angles=0.004 89.812 0.013
+address=1
+[ring_base]
+classname=ring_base_ancient
+position=-12817.029297 11746.117188 -1402.300293
+angles=0.000 -11.758 -0.000
+address=3
+[ramp]
+classname=ramp
+position=2654.407471 -5747.097168 -8347.532227
+angles=-0.044 179.962 -0.025
+model=models/boba_fett/ramps/ramp12.mdl
+[ramp]
+classname=ramp
+position=-6228.604004 -8718.229492 4431.927734
+angles=-1.835 153.379 0.292
+model=models/iziraider/ramp3/ramp3.mdl
+[ramp]
+classname=ramp
+position=6264.505859 5343.086914 6193.503906
+angles=-2.910 -19.900 -0.573
+model=models/boba_fett/ramps/ramp7.mdl
+[ramp]
+classname=sgu_ramp
+position=-2489.985840 -2090.573242 -2956.291992
+angles=-0.000 -35.317 0.000
+model=models/markjaw/sgu_ramp.mdl
+[prop_physics]
+classname=prop_physics
+position=5050.418457 -496.618958 3126.727783
+angles=-60.080 -21.687 -1.385
+model=models/cebt/sga_pwnode.mdl
+__id=609
+[prop_physics]
+classname=prop_physics
+position=5267.833496 -587.270569 3127.072021
+angles=59.906 -24.079 -1.377
+model=models/cebt/sga_pwnode.mdl
+__id=609
+[prop_physics]
+classname=prop_physics
+position=5158.140137 -545.076782 2922.928223
+angles=-0.087 -22.886 179.309
+model=models/cebt/sga_pwnode.mdl
+__id=609

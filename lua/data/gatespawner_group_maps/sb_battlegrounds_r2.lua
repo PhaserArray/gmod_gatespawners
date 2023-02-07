dda5f579c0b592cf3493694e26f64eb779519b91
PhaserArray <me@phaserarray.me>
2023-02-07 15:32:50 +0200

    Add sb_battlegrounds_r2

---
+++ lua/data/gatespawner_group_maps/sb_battlegrounds_r2.lua
@@ -0,0 +1,120 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=stargate_sg1
+position=-14330.195313 -12559.495117 9825.382813
+angles=0.003 -0.000 -0.002
+address=ZARKOL
+group=M@
+name=Zarkol
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=6179.384766 8235.717773 9560.072266
+angles=0.000 -63.420 0.000
+address=CYDONI
+group=M@
+name=Cydonia
+private=false
+locale=false
+[stargate]
+classname=stargate_universe
+position=7114.993652 6351.500000 -7676.123047
+angles=0.018 -26.638 -0.023
+address=KELTAS
+group=U@!
+name=Keltash
+private=false
+locale=false
+[stargate]
+classname=stargate_universe
+position=-8482.954102 6378.092285 1717.905273
+angles=0.000 25.826 0.000
+address=AYRICE
+group=U@!
+name=Aura
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=-8113.168945 -8362.437500 -11156.000000
+angles=0.000 228.689 0.000
+address=RHOD0N
+group=P@
+name=Rhodon
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=10801.598633 -11479.301758 -4998.489746
+angles=0.000 180.788 0.000
+address=KASTIE
+group=P@
+name=Kastiel
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=1109.733887 11479.041016 3856.706543
+angles=0.000 -33.666 0.000
+address=NAVKIR
+group=P@
+name=Navkir Spacegate
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=-11092.943359 -1229.261108 6742.310547
+angles=0.000 75.841 0.000
+address=FREDON
+group=P@
+name=Freedonia
+private=false
+locale=false
+[dhd]
+classname=dhd_sg1
+position=6412.304688 8054.534668 9423.947266
+angles=15.000 -118.091 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-14142.944336 -12396.104492 9718.117188
+angles=15.000 -179.677 -0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=-8585.730469 -8658.993164 -11262.379883
+angles=15.000 -148.846 0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=10512.184570 -11630.981445 -5135.995117
+angles=15.000 -143.501 0.000
+destroyed=false
+[dhd]
+classname=dhd_city
+position=-10731.656250 -281.149963 6881.101074
+angles=0.000 59.376 0.000
+[ramp]
+classname=ramp
+position=-14265.202148 -12559.500977 9680.379883
+angles=0.003 -0.000 -0.002
+model=models/boba_fett/ramps/ramp2.mdl
+[ramp]
+classname=ramp
+position=6179.384766 8235.717773 9341.072266
+angles=0.000 -63.420 0.000
+model=models/boba_fett/ramps/ramp5.mdl
+[ramp]
+classname=sgu_ramp
+position=7116.781738 6350.603516 -7675.123535
+angles=0.018 -26.638 -0.023
+model=models/markjaw/sgu_ramp.mdl
+[ramp]
+classname=sgu_ramp
+position=-8481.154297 6378.963379 1718.905273
+angles=-0.000 25.826 0.000
+model=models/markjaw/sgu_ramp.mdl

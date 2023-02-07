2a503b472ec0cbef3eb6b0f1fab228c8f19ba454
PhaserArray <me@phaserarray.me>
2023-02-07 21:57:20 +0200

    add sb_nrdgalaxy gatespawner

---
+++ garrysmod/lua/data/gatespawner_group_maps/sb_nrdgalaxy.lua
@@ -0,0 +1,72 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=stargate_atlantis
+position=-10583.421875 8707.999023 -8696.105469
+angles=-0.000 90.819 0.000
+address=GAMORI
+group=P@
+name=Gammoria
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=9877.073242 11870.377930 -11040.000000
+angles=0.000 270.801 0.000
+address=ICEWRL
+group=P@
+name=Ice World
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-9759.922852 -12465.141602 -6089.088379
+angles=-0.000 135.383 0.000
+address=XSROCK
+group=M@
+name=Moon
+private=false
+locale=false
+[stargate]
+classname=stargate_tollan
+position=7221.354980 -7941.835938 -9302.619141
+angles=0.000 -90.000 0.000
+address=NRDFST
+group=M@
+name=NrdForest
+private=false
+locale=false
+__id=265
+[dhd]
+classname=dhd_atlantis
+position=-10484.413086 9207.871094 -8789.267578
+angles=15.000 49.762 0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=9692.459961 11548.426758 -11145.305664
+angles=15.000 -117.354 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-10007.484375 -12449.610352 -6194.838379
+angles=15.000 -44.385 0.000
+destroyed=false
+[mobile_dhd]
+classname=mobile_dhd
+position=7336.469727 -8475.435547 -9183.459961
+angles=-0.000 -116.732 0.000
+model=models/props_c17/computer01_keyboard.mdl
+[ramp]
+classname=ramp
+position=-9806.191406 -12419.488281 -6234.088379
+angles=-0.000 135.383 0.000
+model=models/boba_fett/ramps/ramp2.mdl
+[prop_physics]
+classname=prop_physics
+position=7345.674805 -8463.655273 -9183.322266
+angles=-0.372 -113.439 -0.623
+model=models/props_lab/monitor02.mdl
+__id=265

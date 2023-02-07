db934da745080ae2a2ddc77c2eff60f96a5650be
PhaserArray <me@phaserarray.me>
2023-02-07 23:35:37 +0200

    add gm_battlestation gatespawner

---
+++ garrysmod/lua/data/gatespawner_group_maps/gm_battlestation.lua
@@ -0,0 +1,71 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=stargate_atlantis
+position=9434.367188 -9360.378906 -10697.742188
+angles=0.046 16.443 0.004
+address=ASTERD
+group=P@
+name=ss
+private=false
+locale=false
+__id=137
+[stargate]
+classname=stargate_atlantis
+position=-844.632690 0.216298 3178.031250
+angles=0.000 359.903 0.000
+address=STIONH
+group=P@
+name=M0L-69W
+private=false
+locale=false
+[dhd]
+classname=dhd_atlantis
+position=-629.680176 212.458328 3073.231201
+angles=15.000 46.021 0.000
+destroyed=false
+[ring_base]
+classname=ring_base_goauld
+position=0.103251 -0.203300 5360.031250
+angles=0.000 273.512 0.000
+address=0
+[ring_base]
+classname=ring_base_goauld
+position=1.485706 -778.074890 4624.031250
+angles=0.000 90.287 0.000
+address=1
+[ring_base]
+classname=ring_base_goauld
+position=1.362530 -893.324219 4144.031250
+angles=0.000 91.070 0.000
+address=2
+[ring_base]
+classname=ring_base_goauld
+position=844.518311 -0.400808 3616.031250
+angles=0.000 190.719 0.000
+address=3
+[ring_base]
+classname=ring_base_goauld
+position=845.577576 0.025832 3088.031250
+angles=0.000 190.434 0.000
+address=4
+[prop_physics]
+classname=prop_physics
+position=9434.259766 -9360.401367 -10833.740234
+angles=0.004 -73.557 179.954
+model=models/cebt/sga_pwnode.mdl
+__id=137
+[prop_physics]
+classname=prop_physics
+position=9469.035156 -9472.952148 -10629.751953
+angles=60.004 -73.637 -0.092
+model=models/cebt/sga_pwnode.mdl
+__id=137
+[prop_physics]
+classname=prop_physics
+position=9401.361328 -9247.323242 -10629.736328
+angles=-59.996 -73.477 -0.092
+model=models/cebt/sga_pwnode.mdl
+__id=137

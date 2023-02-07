8f1c1a5cc45e698a60fe9cef55611097e4bdb98a
PhaserArray <me@phaserarray.me>
2023-02-07 21:57:43 +0200

    clone sb_origins_v2 gatespawner for sb_origins

---
+++ garrysmod/lua/data/gatespawner_group_maps/sb_origins.lua
@@ -0,0 +1,116 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=stargate_universe
+position=-10421.113281 10662.106445 11190.000000
+angles=0.000 166.662 0.000
+address=LAVRE3
+group=U@!
+name=Reeze
+private=false
+locale=false
+[stargate]
+classname=stargate_universe
+position=8731.256836 6832.318359 4925.000000
+angles=-0.000 0.580 0.000
+address=NYITAE
+group=U@!
+name=Nuyitae
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-3192.838623 -6497.252930 5144.000000
+angles=0.000 92.638 0.000
+address=KOB0LG
+group=M@
+name=Kobol
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=-2634.396729 9883.228516 -5212.968750
+angles=0.000 1.392 0.000
+address=KORHAL
+group=P@
+name=Korhal
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=1483.303223 -2144.450439 -2316.830811
+angles=0.000 160.539 0.000
+address=KORSUN
+group=P@
+name=Korhal Sun
+private=false
+locale=false
+__id=528
+[stargate]
+classname=stargate_atlantis
+position=7360.616699 -12369.672852 12005.015625
+angles=-0.000 90.000 0.000
+address=KOBSUN
+group=P@
+name=Kobol Sun
+private=false
+locale=false
+__id=545
+[dhd]
+classname=dhd_sg1
+position=-3093.929932 -6097.239746 5038.220703
+angles=15.000 75.958 0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=-1746.964844 9892.402344 -5317.768555
+angles=15.000 0.109 0.000
+destroyed=false
+[ramp]
+classname=sgu_ramp
+position=-10523.281250 10686.329102 11094.000000
+angles=0.000 166.662 0.000
+model=models/iziraider/sguramp/sgu_ramp.mdl
+[ramp]
+classname=sgu_ramp
+position=8823.452148 6833.251465 4783.000000
+angles=0.000 0.580 -0.000
+model=models/boba_fett/ramps/sgu_ramp/sgu_ramp_small.mdl
+[prop_physics]
+classname=prop_physics
+position=1521.289185 -2032.959229 -2248.832031
+angles=60.000 70.539 0.000
+model=models/cebt/sga_pwnode.mdl
+__id=528
+[prop_physics]
+classname=prop_physics
+position=1443.789307 -2255.402100 -2248.831787
+angles=-60.000 70.539 0.000
+model=models/cebt/sga_pwnode.mdl
+__id=528
+[prop_physics]
+classname=prop_physics
+position=1483.303223 -2144.450439 -2452.828369
+angles=0.000 70.539 180.000
+model=models/cebt/sga_pwnode.mdl
+__id=528
+[prop_physics]
+classname=prop_physics
+position=7360.616699 -12369.672852 11869.017578
+angles=-0.000 -0.000 180.000
+model=models/cebt/sga_pwnode.mdl
+__id=545
+[prop_physics]
+classname=prop_physics
+position=7478.393555 -12368.344727 12073.014648
+angles=60.000 -0.000 0.000
+model=models/cebt/sga_pwnode.mdl
+__id=545
+[prop_physics]
+classname=prop_physics
+position=7242.838379 -12369.381836 12073.014648
+angles=-60.000 -0.000 -0.000
+model=models/cebt/sga_pwnode.mdl
+__id=545
